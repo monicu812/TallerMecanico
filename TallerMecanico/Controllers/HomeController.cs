d4ad026b457898c1fba5914f66f00e0ae2d4935f
DESKTOP-CK1OG35\User <monicu81@gmail.com>
2022-08-31 17:17:36 -0500

    Agregar archivos de proyecto.

---
+++ TallerMecanico/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TallerMecanico.Models;
+
+namespace TallerMecanico.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+        public IActionResult Empleado()
+        {
+            return View();
+        }
+
+        public IActionResult Vehiculo()
+        {
+            return View();
+        }
+
+               
+        public IActionResult Historial()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TallerMecanico/Controllers/InicioSesionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TallerMecanico.Controllers
+{
+    public class InicioSesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
